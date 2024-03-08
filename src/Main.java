72daa0d07b96df3c63c66c030294bfbd4b3e7898
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-08 22:37:27 +0300

    adjustment have been made.

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
         System.out.println("Homework 8.");
         System.out.println("Task 1.");
         int [] num = new int [3];
-        num [0] = 1;
-        num [1] = 2;
-        num [2] = 3;
-        double [] num1 = {1.57, 7.564, 9.986};
-        int [] num2 = {11,22,33};
+        num [0]=1;
+        num [1]=2;
+        num [2]=3;
+        double [] num1={1.57, 7.564, 9.986};
+        int [] num2={11,22,33};
         System.out.println(" ");
         System.out.println("Task 2.");
         System.out.print(Arrays.toString(num));
@@ -19,21 +19,26 @@ public class Main {
         System.out.println(Arrays.toString(num2));
         System.out.println(" ");
         System.out.println("Task 3.");
-        for (int i = num.length - 1; i >= 0; i--) {
+        for (int i=num.length - 1; i>=0; i--) {
             System.out.print(num[i] + ", ");
         }
         System.out.println(" ");
-        for (int i = num1.length - 1; i >= 0; i--) {
+        for (int i=num1.length - 1; i>=0; i--) {
             System.out.print(num1[i] + ", ");
         }
         System.out.println(" ");
-        for (int i = num2.length - 1; i >= 0; i--){
+        for (int i=num2.length - 1; i>=0; i--){
             System.out.print(num2[i] + ", ");
         }
         System.out.println(" ");
         System.out.println("Task 4.");
-        num [0] = num [0] + 1;
-        num [2] = num [2] + 1;
+        //num [0] = num [0] + 1;
+        //num [2] = num [2] + 1;
+        for (int q=0; q<num.length; q++){
+            if (num[q]%2!=0){
+                num[q]++;
+            }
+        }
         System.out.print(Arrays.toString(num));
     }
 }
