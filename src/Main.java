0e4f8c5a7685c249592965e3f3c12bc7172938f0
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-08 16:18:26 +0300

    task 2 completed.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Homework 8.");
@@ -6,19 +8,14 @@ public class Main {
         num [0] = 1;
         num [1] = 2;
         num [2] = 3;
-        System.out.println(num[2]);
-        System.out.println(num[1]);
-        System.out.println(num[0]);
         double [] num1 = {1.57, 7.564, 9.986};
-        System.out.println(num1[2]);
-        System.out.println(num1[0]);
-        System.out.println(num1[1]);
         int [] num2 = {11,22,33};
-        System.out.println(num2[0]);
-        System.out.println(num2[1]);
-        System.out.println(num2[2]);
         System.out.println(" ");
         System.out.println("Task 2.");
-
+        System.out.print(num[0] + ", " + num[1] + ", " + num[2]);
+        System.out.println(" ");
+        System.out.print(num1[0] + ", " + num1[1] + ", " + num1[2]);
+        System.out.println(" ");
+        System.out.println(Arrays.toString(num2));
     }
 }
