c5508dc9fa71a5258a66fbfb569e1f35f4cc2c15
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-08 15:35:45 +0300

    homework 8. task 1.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] num = new int [3];
+        num [0] = 1;
+        num [1] = 2;
+        num [2] = 3;
+        System.out.println(num[2]);
+        System.out.println(num[1]);
+        System.out.println(num[0]);
+    }
+}
