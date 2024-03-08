e28507a73e9ff3448f84706534805d1a9421da9f
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-08 16:00:58 +0300

    task 1 completed.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Homework 8.");
+        System.out.println("Task 1.");
         int [] num = new int [3];
         num [0] = 1;
         num [1] = 2;
@@ -7,5 +9,16 @@ public class Main {
         System.out.println(num[2]);
         System.out.println(num[1]);
         System.out.println(num[0]);
+        double [] num1 = {1.57, 7.564, 9.986};
+        System.out.println(num1[2]);
+        System.out.println(num1[0]);
+        System.out.println(num1[1]);
+        int [] num2 = {11,22,33};
+        System.out.println(num2[0]);
+        System.out.println(num2[1]);
+        System.out.println(num2[2]);
+        System.out.println(" ");
+        System.out.println("Task 2.");
+
     }
 }
