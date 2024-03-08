a0bb89227e02a5fa57d82fec879281e83d65299e
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-08 16:51:44 +0300

    homework 8 completed.

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,28 @@ public class Main {
         int [] num2 = {11,22,33};
         System.out.println(" ");
         System.out.println("Task 2.");
-        System.out.print(num[0] + ", " + num[1] + ", " + num[2]);
+        System.out.print(Arrays.toString(num));
         System.out.println(" ");
-        System.out.print(num1[0] + ", " + num1[1] + ", " + num1[2]);
+        System.out.print(Arrays.toString(num1));
         System.out.println(" ");
         System.out.println(Arrays.toString(num2));
+        System.out.println(" ");
+        System.out.println("Task 3.");
+        for (int i = num.length - 1; i >= 0; i--) {
+            System.out.print(num[i] + ", ");
+        }
+        System.out.println(" ");
+        for (int i = num1.length - 1; i >= 0; i--) {
+            System.out.print(num1[i] + ", ");
+        }
+        System.out.println(" ");
+        for (int i = num2.length - 1; i >= 0; i--){
+            System.out.print(num2[i] + ", ");
+        }
+        System.out.println(" ");
+        System.out.println("Task 4.");
+        num [0] = num [0] + 1;
+        num [2] = num [2] + 1;
+        System.out.print(Arrays.toString(num));
     }
 }
